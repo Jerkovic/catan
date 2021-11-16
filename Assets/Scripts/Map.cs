af5417d0b400c5825a24dee84625b1b32d24dfef
Jerkovic <erilundin@gmail.com>
2021-11-16 15:23:05 +0100

    dev progress

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -10,6 +10,9 @@ public class Map : MonoBehaviour {
 	private void Start ()
 	{
 		board = new Board(3);
+		/*
+		 * corners 54 edges 72 tiles 19 chits 18 ports 9
+		 */
 		
 		foreach (var tile in board.tiles)
 		{			
@@ -30,31 +33,58 @@ public class Map : MonoBehaviour {
 		initCorner(206839, 206861, 207367);
 		initCorner(206839, 207367, 207345);
 		
-		// next ring
+		// next ring total #18
 		initCorner(206817, 207345, 207323);
 		initCorner(206817, 207323, 206795);
 		initCorner(206817, 206795, 206289);		
-		initCorner(206817, 206289, 206311);
-		
+		initCorner(206817, 206289, 206311);		
 		initCorner(206311, 206289, 205783);
 		initCorner(206311, 205783, 205805);
 		initCorner(206311, 206333, 205805);
 		initCorner(206333, 205805, 205827);
 		initCorner(206333, 205827, 206355);
-		initCorner(206333, 206355, 206861);
-		
+		initCorner(206333, 206355, 206861);		
 		initCorner(206861, 206355, 206883);
 		initCorner(206861, 206883, 207389);		
-		initCorner(206861, 207367, 207389);
-		
+		initCorner(206861, 207367, 207389);		
 		initCorner(207367, 207389, 207895);
 		initCorner(207367, 207895, 207873);
 		initCorner(207367, 207873, 207345);
 		initCorner(207345, 207873, 207851);
 		initCorner(207345, 207851, 207323);		
-		// End 2:nd ring
+		// End 2:nd ring  
 		
-		
+		// Start 3 ring # of corners 30
+		initCorner(206795, 207323, 207301); 
+		initCorner(206795, 207301, 206773); 
+		initCorner(206267, 206795, 206773);
+		initCorner(206289, 206795, 206267);
+		initCorner(205761, 206289, 206267);
+		initCorner(205783, 205761, 206289);
+		initCorner(205255, 205783, 205761);
+		initCorner(205277, 205783, 205255);
+		initCorner(205805, 205277, 205783); 
+		initCorner(205299, 205805, 205277);
+		initCorner(205299, 205805, 205827); 
+		initCorner(205321, 205827, 205299); 
+		initCorner(205321, 205827, 205849); 		
+		initCorner(206355, 205827, 205849); 
+		initCorner(206355, 205849, 206377); 
+		initCorner(206355, 206377, 206883);
+		initCorner(206883, 206377, 206905);
+		initCorner(206883, 207411, 206905);			
+		initCorner(206883, 207411, 207389); 
+		initCorner(207389, 207917, 207411);
+		initCorner(207389, 207917, 207895);
+		initCorner(207895, 208423, 207917);		
+		initCorner(207895, 208423, 208401);
+		initCorner(207895, 207873, 208401);
+		initCorner(207873, 208401, 208379);
+		initCorner(207873, 207851, 208379);
+		initCorner(207851, 208379, 208357); 
+		initCorner(207851, 208357, 207829);
+		initCorner(207323, 207851, 207829);
+		initCorner(207323, 207829, 207301);
 		
 	}
 
--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,10 +1,21 @@
-﻿using EventSystem;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EventSystem;
 using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
 	[SerializeField]
 	private new Camera camera;
+
+	private List<int> outer;
+
+	private void Start()
+	{
+		outer = new List<int>();		
+		Debug.Log(outer.Count);
+	}
 
 	private void Update () {
 		if (Input.GetMouseButtonDown(0)) {
@@ -19,12 +30,28 @@ public class MouseManager : MonoBehaviour
 				{
 					// Events.OnClickHexagon.Invoke(int.Parse(ourHitObject.name));
 					// move to controller 
-					Debug.Log("Clicked On: " + ourHitObject.name);
+					// Debug.Log("Clicked On: " + ourHitObject.name);
+					outer.Add(int.Parse(ourHitObject.name));
 					MeshRenderer mr = ourHitObject.GetComponentInChildren<MeshRenderer>();
 					mr.material.color = Color.red;
 				}
 				 	
 			}
 		}
+	
+		if (Input.GetMouseButtonDown(1))
+		{
+			var result = "";
+			Debug.Log(outer.Count);
+			var i = 0;
+			foreach (var item in outer)
+			{
+				result += item + ", ";
+				i++;
+				if (i % 3 == 0) result += "\n";
+			}
+			Debug.Log(result);
+		}
 	}
+	
 }
