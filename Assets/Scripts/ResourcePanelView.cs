3a08b20f65ae2aade923f72018d9c47f2a7a0b2b
Jerkovic <erilundin@gmail.com>
2021-11-21 15:55:19 +0100

    dev progress

--- Assets/Scripts/ResourcePanelView.cs
+++ Assets/Scripts/ResourcePanelView.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class ResourcePanelView : MonoBehaviour
 {
     // should be listening on ResourceUpdateEvent
+    private TextMeshProUGUI _woodText;
+    private TextMeshProUGUI _brickText;
+    private TextMeshProUGUI _sheepText;
+    private TextMeshProUGUI _wheatText;
+    private TextMeshProUGUI _oreText;
+    
     private void Start()
     {
-        // todo cache
-        var wood = transform.GetChild(0);
-        var text = wood.GetComponentsInChildren<TextMeshProUGUI>();
-        text[0].SetText("12");
+        _woodText = transform.GetChild(0).GetComponentsInChildren<TextMeshProUGUI>()[0];
+        _brickText = transform.GetChild(1).GetComponentsInChildren<TextMeshProUGUI>()[0];
+        _sheepText = transform.GetChild(2).GetComponentsInChildren<TextMeshProUGUI>()[0];
+        _wheatText = transform.GetChild(3).GetComponentsInChildren<TextMeshProUGUI>()[0];
+        _oreText = transform.GetChild(4).GetComponentsInChildren<TextMeshProUGUI>()[0];
+
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        _woodText.SetText("1");
+        _brickText.SetText("2");
+        _sheepText.SetText("4");
+        _wheatText.SetText("5");
+        _oreText.SetText("8");
     }
 }
