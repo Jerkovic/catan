d0aae7ff06d1aad84a97eb0c5634c1c29ebe4fa4
Jerkovic <erilundin@gmail.com>
2021-11-28 18:13:59 +0100

    Cached event system

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,42 +1,47 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using EventSystem;
+﻿using EventSystem;
 using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
-	[SerializeField] private new Camera camera;
+    [SerializeField] private new Camera camera;
 
-	private void Update()
-	{
-		if (Input.GetMouseButtonDown(0))
-		{
-			if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
-			{
-				return;
-			}
+    private UnityEngine.EventSystems.EventSystem _cachedEventSystem;
 
-			var ray = camera.ScreenPointToRay(Input.mousePosition);
+    private void Awake()
+    {
+        _cachedEventSystem = UnityEngine.EventSystems.EventSystem.current;
+    }
 
-			if (Physics.Raycast(ray, out var hitInfo))
-			{
-				var ourHitObject = hitInfo.collider.transform;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_cachedEventSystem.IsPointerOverGameObject())
+            {
+                return;
+            }
 
-				if (ourHitObject != null && ourHitObject.CompareTag("HexagonMesh"))
-				{
-					Events.OnClickHexagon.Invoke(ourHitObject.parent.gameObject);
-				}
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
 
-				if (ourHitObject != null && ourHitObject.CompareTag("Edge"))
-				{
-					Events.OnClickEdge.Invoke(ourHitObject.gameObject);
-				}
+            if (Physics.Raycast(ray, out var hitInfo))
+            {
+                var ourHitObject = hitInfo.collider.transform;
 
-				if (ourHitObject != null && ourHitObject.CompareTag("Corner"))
-				{
-					Events.OnClickCorner.Invoke(ourHitObject.gameObject);
-				}
-			}
-		}
-	}
+                if (ourHitObject != null && ourHitObject.CompareTag("HexagonMesh"))
+                {
+                    Events.OnClickHexagon.Invoke(ourHitObject.parent.gameObject);
+                }
+
+                if (ourHitObject != null && ourHitObject.CompareTag("Edge"))
+                {
+                    Events.OnClickEdge.Invoke(ourHitObject.gameObject);
+                }
+
+                if (ourHitObject != null && ourHitObject.CompareTag("Corner"))
+                {
+                    Events.OnClickCorner.Invoke(ourHitObject.gameObject);
+                }
+            }
+        }
+    }
 }
