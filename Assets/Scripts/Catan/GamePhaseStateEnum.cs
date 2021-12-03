85f9a28428c343fc8c794b2ca08a175f4ab61c2c
Erik Lundin <erilundin@gmail.com>
2021-12-03 08:50:55 +0100

    Added game phase states enum

---
+++ Assets/Scripts/Catan/GamePhaseStateEnum.cs
@@ -0,0 +1,9 @@
+namespace Catan
+{
+    public enum GamePhaseStateEnum  : int
+    {
+        PLACE_FIRST_SETTLEMENT_ROAD  = 0,
+        PLACE_SECOND_SETTLEMENT_ROAD  = 1, // second phase settlement will produce resources
+    }
+}
+
